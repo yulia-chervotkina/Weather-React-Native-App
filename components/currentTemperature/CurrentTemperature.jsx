c03c662a95f461977b7cca28633f7050e6caee92
Yulia <jchervotkina@gmail.com>
2025-08-27 12:23:34 +0200

    Adding logic to render current weather

--- components/currentTemperature/CurrentTemperature.jsx
+++ components/currentTemperature/CurrentTemperature.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Image } from 'react-native';
-import styles from './styles'
+import styles from './styles';
+import useWeatherStore from '../../stores/weatherStore.js';
 
 const CurrentTemperature = () => {
+  const {weather, fetchWeather, error, loading} = useWeatherStore();
+  
+  useEffect(() => {
+    fetchWeather('Cracow')
+  }, [fetchWeather]);
+
+  if (loading) return <Text>Loading ...</Text>
+  if (error) return <Text>Error: {error}</Text>;
+  if (!weather) return <Text>Couldn't fetch data</Text>
+
+  const iconURL = 'https:' + weather.current.condition.icon;
+  const {condition, temp_c} = weather.current;
+
   return (
     <View style={styles.currentTemperature}>
-      <Text>23°</Text>
-        (// <Image />)
-      <Text>Partially cloudy</Text>
+      <Text>{condition.text}</Text>
+      <Image 
+        source={{uri: iconURL}} 
+        style={{ width: 64, height: 64 }}/>
+      <Text>{temp_c}</Text>
     </View>
   )
 };
--- components/currentWeatherData/CurrentWeatherData.jsx
+++ components/currentWeatherData/CurrentWeatherData.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
-import styles from './styles'
+import styles from './styles';
+import useWeatherStore from '../../stores/weatherStore.js';
 
 const CurrentWeatherData = () => {
-  let UV = '7';
-  let humidity = '61%';
-  let percipitation = '4mm';
-  let wind = '5';
+  const { weather, fetchWeather, error, loading } = useWeatherStore();
 
-  return (
+  useEffect(() => {
+      fetchWeather('London')
+    }, [fetchWeather]);
+
+  if (loading) return <Text>Loading ...</Text>
+  if (error) return <Text>Error: {error}</Text>;
+  if (!weather) return <Text>Couldn't fetch data</Text>;
+
+  const { uv, humidity, precip_mm, wind_kph } = weather.current;
+
+    return (
     <View style={styles.currentWeatherData}>
       <View style={styles.currentWeatherDataItem}>
         <Text>UV Index</Text>
-        <Text>{UV}</Text>
+        <Text>{uv}</Text>
       </View>
       <View style={styles.currentWeatherDataItem}>
         <Text>Humidity</Text>
@@ -20,11 +28,11 @@ const CurrentWeatherData = () => {
       </View>
       <View style={styles.currentWeatherDataItem}>
         <Text>Percipitation</Text>
-        <Text>{percipitation}</Text>
+        <Text>{precip_mm}</Text>
       </View>
       <View style={styles.currentWeatherDataItem}>
         <Text>Wind</Text>
-        <Text>{wind}</Text>
+        <Text>{wind_kph}</Text>
       </View>
     </View>
   )
