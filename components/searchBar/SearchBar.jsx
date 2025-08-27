b997863834ef48c9b2a001602eded5f1cf0fd651
Yulia <jchervotkina@gmail.com>
2025-08-27 12:25:26 +0200

    Adding SearchBar with a FlatList to get Autocomplete data and render on scree

--- components/searchBar/SearchBar.jsx
+++ components/searchBar/SearchBar.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import { TextInput } from 'react-native';
-import styles from './styles'
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, TextInput, Text, TouchableOpacity } from 'react-native';
+import styles from './styles';
+import useLocationStore from '../../stores/locationStore.js'
 
 const SearchBar = () => {
+  const [location, setLocation] = useState('');
+  const { result, searchLocation, loading, error } = useLocationStore();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      searchLocation(location)
+    }, 400);
+    return () => clearTimeout(timeout);
+  }, [location, searchLocation]);
+
   return (
-  <TextInput 
-    style={styles.searchBar} 
-    placeholder="Type here to find a city!"
-    />
+  <View>
+    <TextInput 
+      style={styles.searchBar} 
+      value={location}
+      placeholder="Type here to find a city!"
+      onChangeText={setLocation}
+      />
+      {loading && <Text>Loading...</Text>}
+      {error && <Text>{error}</Text>}
+    <FlatList 
+      data={result || []} 
+      renderItem={({item, index}) => {
+        return(
+        <TouchableOpacity
+          key={index.toString()} 
+          onPress={console.log("hi")}
+        >
+          <Text>{item.name},{item.country}</Text>
+        </TouchableOpacity>
+        )
+      }}
+      keyExtractor={item=>item.id}
+      />
+  </View>
   )
 };
 
-export default SearchBar
+export default SearchBar;
