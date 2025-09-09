33d3d2934f5782baeb9fc6a268263052446ab140
Yulia <jchervotkina@gmail.com>
2025-09-09 20:46:51 +0200

    Added Linear Gradient on Homescreen + improved styling

--- screens/Home/HomeScreen.jsx
+++ screens/Home/HomeScreen.jsx
@@ -4,14 +4,11 @@ import styles from './styles.js';
 import {useNavigation} from '@react-navigation/native';
 import { Button } from '@react-navigation/elements';
 import useLocationStore from '../../stores/locationStore.js';
-
-// import SearchBar from '../SavedLocations/searchBar/SearchBar.jsx';
-import CurrentLocation from './components/currentLocation/CurrentLocation';
-import CurrentTemperature from './components/currentTemperature/CurrentTemperature';
-// import CurrentWeatherData from './components/currentWeatherData/CurrentWeatherData';
-import HourlyForecast from './components/hourlyForecast/HourlyForecast';
-// import TenDayForecast from './components/tenDayForecast/TenDayForecast';
 import LinearGradient from 'react-native-linear-gradient';
+
+import CurrentTemperature from './components/currentTemperature/CurrentTemperature';
+import HourlyForecast from './components/hourlyForecast/HourlyForecast';
+import TenDayForecast from './components/tenDayForecast/TenDayForecast';
 
 const HomeScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
@@ -27,27 +24,24 @@ const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <LinearGradient useAngle={true} angle={45} colors={['#47BFDF', '#4A91FF']} style={styles.linearGradient}>
-    <ScrollView 
-      contentContainerStyle={styles.container}
-      refreshControl={
-        <RefreshControl 
-        refreshing={refreshing}
-        onRefresh={onRefresh}
-        colors={['grey']}
-        tintColor='black'
-        bounces={!refreshing}
-        />
-      }
-      >
-      <Button onPress={() => navigation.navigate('Saved')}>{city}</Button>
-      {/* <SearchBar /> */}
-      <CurrentLocation />
-      <CurrentTemperature />
-      {/* <CurrentWeatherData /> */}
-      <HourlyForecast />
-      {/* <TenDayForecast /> */}
-    </ScrollView>
+    <LinearGradient useAngle={true} angle={45} colors={['#4A91FF', '#47BFDF']} style={styles.linearGradient}>
+      <ScrollView 
+        contentContainerStyle={styles.container}
+        refreshControl={
+          <RefreshControl 
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          colors={['grey']}
+          tintColor='black'
+          bounces={!refreshing}
+          />
+        }
+        >
+        <Button onPress={() => navigation.navigate('Saved')} style={styles.button}>{city}</Button>
+        <CurrentTemperature />
+        <HourlyForecast />
+        <TenDayForecast />
+      </ScrollView>
     </LinearGradient>
   )
 };
--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -3,18 +3,29 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    // flex: 1,
-    paddingTop: 100,
-    backgroundColor: 'rgb(44 156 214)',
     paddingLeft: '20',
     paddingRight: '20',
   },
   linearGradient: {
     flex: 1,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderRadius: 5
-  }
+    borderRadius: 5,
+  },
+  button: {
+    // backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 1.5,
+    borderRadius: 20,
+    margin: 10,
+    shadowColor: 'lime',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 15,
+  },
 });
 
 export default styles;
