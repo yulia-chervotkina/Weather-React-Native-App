dd0d9342b57ae7106348578a7e021d0cad512611
Yulia <jchervotkina@gmail.com>
2025-09-09 20:52:19 +0200

    Added Linear Gradient on SavedLocationsScreen + improved styling

--- screens/SavedLocations/SavedLocationsScreen.jsx
+++ screens/SavedLocations/SavedLocationsScreen.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View }  from 'react-native';
 import SearchBar from './searchBar/SearchBar.jsx';
-import SavedLocations from './savedLocations/SavedLocations.jsx'
+import SavedLocations from './savedLocations/SavedLocations.jsx';
 // import useSavedLocationsStore from '../../../stores/savedLocationsStore.js';
-
 // import { useNavigation } from '@react-navigation/native';
+import LinearGradient from 'react-native-linear-gradient';
+import styles from './styles.js';
 
 const SavedLocationsScreen = () => {
 //   const navigation = useNavigation();
     
   return (
-    <View>
+    <LinearGradient useAngle={true} angle={45} colors={['#4A91FF', '#47BFDF']} style={styles.linearGradient}>
+    <View style={styles.container}>
       <SearchBar />
       <SavedLocations />
     </View>
+    </LinearGradient>
   );
 }
 
---
+++ screens/SavedLocations/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: '20',
+  },
+});
+
+export default styles;
