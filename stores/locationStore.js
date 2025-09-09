cadf14f51b6de13bdd6983d72fd1f96566752361
Yulia <jchervotkina@gmail.com>
2025-09-09 20:44:42 +0200

    Adding Async Storage to Zustand stores

--- stores/locationStore.js
+++ stores/locationStore.js
@@ -1,8 +1,17 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const useLocationStore = create(set => ({
-  city: null,
-  setCity: (newCity) => set({ city: newCity }),
-}));
-
+const useLocationStore = create()(
+  persist(
+    set => ({
+      city: null,
+      setCity: newCity => set({ city: newCity }),
+    }),
+    {
+      name: 'location',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
 export default useLocationStore;
--- stores/savedLocationsStore.js
+++ stores/savedLocationsStore.js
@@ -1,9 +1,19 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const useSavedLocationsStore = create(set => ({
-  savedLocations: [],
-  setSavedLocations: location =>
-    set(state => ({ savedLocations: [...state.savedLocations, location] })),
-}));
+const useSavedLocationsStore = create()(
+  persist(
+    set => ({
+      savedLocations: [],
+      setSavedLocations: location =>
+        set(state => ({ savedLocations: [...state.savedLocations, location] })),
+    }),
+    {
+      name: 'savedLocations',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
 
 export default useSavedLocationsStore;
