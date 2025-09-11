e036e80a39922692f6042e35334562c0b358af29
Yulia <jchervotkina@gmail.com>
2025-09-11 18:13:59 +0200

    Creating a custom Text component

---
+++ components/Text/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { getFontFamily } from '../../utils/fontFamily.js';
+
+const styles = StyleSheet.create({
+  plainText: {
+    fontSize: 18,
+    fontFamily: getFontFamily('normal'),
+    color: 'white',
+    textShadowColor: '#4A91FF',
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 3,
+  },
+});
+
+export default styles;
