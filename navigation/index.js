1f41760d6f4ca0181e87d526f42235f64dbf1a73
Yulia <jchervotkina@gmail.com>
2025-09-17 13:17:05 +0200

    Improving navigation between screens

--- navigation/index.js
+++ navigation/index.js
@@ -1,10 +1,17 @@
 import { createStaticNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/Home/HomeScreen/HomeScreen.jsx';
 import SearchLocationsScreen from '../screens/SearchLocations/SearchLocationsScreen/SearchLocationsScreen.jsx';
 import StarredLocationsScreen from '../screens/StarredLocations/StarredLocationsScreen/StarredLocationsScreen.jsx';
 
-const RootStack = createNativeStackNavigator({
+const RootStack = createStackNavigator({
+  detachInactiveScreens: true,
+  screenOptions: {
+    headerStyle: {
+      backgroundColor: '#47BFDF',
+    },
+    headerTintColor: '#fff',
+  },
   screens: {
     Home: HomeScreen,
     Search: SearchLocationsScreen,
